2265f1eb5d48ca928846163337d3781b7720dff6
gawandeabhishek <abhishekgawande1667@gmail.com>
2024-10-30 02:44:56 +0530

    new commit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -16,15 +16,10 @@ const page = () => {
     {
       src: "/chordly.png", // Image for web development
       title: "Chordly",
-      description:
-        "Responsive music web project with API integration.",
-      badge: [
-        "React.js",
-        "API",
-        "TailwindCSS",
-      ],
+      description: "Responsive music web project with API integration.",
+      badge: ["React.js", "API", "TailwindCSS"],
       timeline: "March 2024 - Working",
-      href: "https://github.com/gawandeabhishek/chordly"
+      href: "https://github.com/gawandeabhishek/chordly",
     },
   ];
 
@@ -35,37 +30,36 @@ const page = () => {
       </h3>
       <div className="flex flex-wrap items-center sm:justify-start justify-center gap-8 w-fit">
         {projects.map((item, index) => (
-          <Link href={item.href}>
-          <Card
-            key={index}
-            className="w-fit h-[34rem] py-4 bg-transparent flex flex-col items-start justify-start border-none"
-          >
-            <CardContent>
-              <Image
-                src={item.src}
-                alt={item.title}
-                width={300}
-                height={300}
-                className="rounded-2xl object-cover"
-              />
-            </CardContent>
-            <CardHeader>
-              <CardTitle className="text-secondary w-64">
-                {item.title}
-              </CardTitle>
-              <CardDescription className="w-64">
-                {item.description}
-              </CardDescription>
-              <CardDescription className="w-64 font-semibold">{item.timeline}</CardDescription>
-            </CardHeader>
-            <CardFooter className="flex flex-wrap justify-start items-center gap-1.5 w-[22rem]">
-              {item.badge.map((badge, index) => (
-                <Badge key={index} variant={"secondary"}>
-                  {badge}
-                </Badge>
-              ))}
-            </CardFooter>
-          </Card>
+          <Link href={item.href} key={index}>
+            <Card className="w-fit h-[34rem] py-4 bg-transparent flex flex-col items-start justify-start border-none">
+              <CardContent>
+                <Image
+                  src={item.src}
+                  alt={item.title}
+                  width={300}
+                  height={300}
+                  className="rounded-2xl object-cover"
+                />
+              </CardContent>
+              <CardHeader>
+                <CardTitle className="text-secondary w-64">
+                  {item.title}
+                </CardTitle>
+                <CardDescription className="w-64">
+                  {item.description}
+                </CardDescription>
+                <CardDescription className="w-64 font-semibold">
+                  {item.timeline}
+                </CardDescription>
+              </CardHeader>
+              <CardFooter className="flex flex-wrap justify-start items-center gap-1.5 w-[22rem]">
+                {item.badge.map((badge, index) => (
+                  <Badge key={index} variant={"secondary"}>
+                    {badge}
+                  </Badge>
+                ))}
+              </CardFooter>
+            </Card>
           </Link>
         ))}
       </div>
