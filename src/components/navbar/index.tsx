d555b083c3aebddba31e874c933ddc1e60ecfe08
gawandeabhishek <abhishekgawande1667@gmail.com>
2024-10-30 13:59:32 +0530

    new commit

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,11 +5,17 @@ import { Button } from "../ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import { Menu, MessagesSquare } from "lucide-react";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const PageNavbar = async () => {
   const user = await currentUser();
   const navIcons = ["home", "about", "projects", "skills", "contact", "chat"];
+  
   const Navbar = (
     <nav className="flex sm:items-center justify-between px-4 py-2 bg-transparent sm:flex-row flex-col sm:gap-0 gap-10">
       <DeveloperIcon
@@ -21,17 +27,31 @@ const PageNavbar = async () => {
       <div className="flex items-center justify-center gap-4 sm:flex-row flex-col">
         {navIcons.map((icon) => (
           <Button key={icon}>
-            <Link href={icon === "home" ? "/" : `/${icon}`} className="flex gap-4 items-center justify-center">{icon === "chat" ? (<><MessagesSquare /> <span className="lg:flex hidden">{icon}</span></>) : <>{icon}</>}</Link>
+            <Link
+              href={icon === "home" ? "/" : `/${icon}`}
+              className="flex gap-4 items-center justify-center"
+            >
+              {icon === "chat" ? (
+                <>
+                  <MessagesSquare />{" "}
+                  <span className="lg:flex hidden">{icon}</span>
+                </>
+              ) : (
+                <>{icon}</>
+              )}
+            </Link>
           </Button>
         ))}
       </div>
-      <span className="md:flex hidden">{!user ? (
-        <Link href={"/auth/sign-in"}>
-          <Button variant={"secondary"}>Sign In</Button>
-        </Link>
-      ) : (
-        <UserButton />
-      )}</span>
+      <span className="md:flex hidden">
+        {!user ? (
+          <Link href={"/auth/sign-in"}>
+            <Button variant={"secondary"}>Sign In</Button>
+          </Link>
+        ) : (
+          <UserButton />
+        )}
+      </span>
     </nav>
   );
 
@@ -48,7 +68,44 @@ const PageNavbar = async () => {
             side={"left"}
             className="p-10 w-full h-full bg-transparent border-none"
           >
-            {Navbar}
+            <nav className="flex sm:items-center justify-between px-4 py-2 bg-transparent sm:flex-row flex-col sm:gap-0 gap-10">
+              <DeveloperIcon
+                fullname="abhishek gawande"
+                email="abhishekgawande1667@gmail.com"
+                image="/abhishek-img.png"
+                alt="@gawandeabhishek"
+              />
+              <div className="flex items-center justify-center gap-4 sm:flex-row flex-col">
+                {navIcons.map((icon) => (
+                  <Button key={icon}>
+                    <SheetClose asChild>
+                      <Link
+                        href={icon === "home" ? "/" : `/${icon}`}
+                        className="flex gap-4 items-center justify-center"
+                      >
+                        {icon === "chat" ? (
+                          <>
+                            <MessagesSquare />{" "}
+                            <span className="lg:flex hidden">{icon}</span>
+                          </>
+                        ) : (
+                          <>{icon}</>
+                        )}
+                      </Link>
+                    </SheetClose>
+                  </Button>
+                ))}
+              </div>
+              <span className="md:flex hidden">
+                {!user ? (
+                  <Link href={"/auth/sign-in"}>
+                    <Button variant={"secondary"}>Sign In</Button>
+                  </Link>
+                ) : (
+                  <UserButton />
+                )}
+              </span>
+            </nav>
           </SheetContent>
         </Sheet>
         {!user ? (
